95da7f07dbbe931093c8b8891cd64d55b7f84d8c
ThawZinToe <toezinthaw@gmail.com>
2022-11-28 22:35:14 +0700

    test ci/cd

--- app/src/main/java/com/ptut/ptmovie/initializer/StartupInitializer.kt
+++ app/src/main/java/com/ptut/ptmovie/initializer/StartupInitializer.kt
@@ -5,6 +5,7 @@ import androidx.startup.Initializer
 
 class StartupInitializer : Initializer<Unit> {
     override fun create(context: Context) {
+
     }
 
     override fun dependencies(): MutableList<Class<out Initializer<*>>> {
